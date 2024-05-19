8545cc8167949ab3b4e7cbd384cbab680475c326
imdatsing <136227712+imdatsing@users.noreply.github.com>
2024-05-20 00:38:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import Model1, Model2, Model3
+import Model1, Model2, Model3, Home
 
-st.set_page_config(
-    page_title="Imdatsing",
-)
+# Set page configuration
+st.set_page_config(page_title="IMDATSING")
 
+# User credentials
+users = {
+    "imdatsing": "imdatsing",
+    "chicken": "chicken"
+}
+
+# Function to check login
+def check_login(username, password):
+    if username in users and users[username] == password:
+        return True
+    return False
+
+# Login page
+def login_page():
+    st.title("Login Page")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if check_login(username, password):
+            st.session_state["logged_in"] = True
+            st.session_state["username"] = username
+            st.experimental_rerun()
+        else:
+            st.error("Invalid username or password")
+
+# Main app class
 class MultiApp:
     def __init__(self):
-        self.apps=[]
+        self.apps = []
+
     def add_app(self, title, function):
-        sef.apps.append({
+        self.apps.append({
             "title": title,
             "function": function
         })
-    
-    def run():
+
+    def run(self):
         with st.sidebar:
             app = option_menu(
-                menu_title='Imdatsing',
-                options=['Model 1', 'Model 2', 'Model 3'],
-                menu_icon='chat-text-fill',
-                default_index=1
+                menu_title='IMDATSING',
+                options=['Home', 'Model 1', 'Model 2', 'Model 3'],
+                icons=['house-fill', 'envelope-open-heart-fill', 'envelope-open-heart-fill', 'envelope-open-heart-fill'],
+                menu_icon='person-arms-up',
+                default_index=0
             )
 
-        if app=='Model 1':
+        if app == 'Model 1':
             Model1.app()
-        if app=='Model 2':
+        elif app == 'Model 2':
             Model2.app()
-        if app=='Model 3':
+        elif app == 'Model 3':
             Model3.app()
-    run()
+        elif app == 'Home':
+            Home.app()
+
+# Check if the user is logged in
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+
+if st.session_state["logged_in"]:
+    app = MultiApp()
+    app.run()
+else:
+    login_page()
